2b1467046c5109d9e5853cf85ebec8210e308dab
depanama <kudiatutomanitu@gmail.com>
2025-06-09 15:20:07 +0100

    denition de la fonction qui verifie si le nombre est premier definition de la fonction qui retourne le tableau

--- tableau/index.js
+++ tableau/index.js
@@ -42,18 +42,34 @@ console.log("voici la liste de nombre impair",impair);
 nbPairImpair([7, 3, 5, 2, 4, 2, 20, 50])
 
 // nombre premier du tableau
-function nombrePremier(tab){
-    for(let i=0;i<tab.length;i++){
-        if(tab[i]<=1){
-            console.log(`${tab[i]} n'est un nombre premier`);            
-        }else if(tab[i]%2===0){
-            console.log(`${tab[i]} n'est pas un nombre premier`);
-            
-        }else{
-            console.log(`${tab[i]} c'est un nombre premier`);
-            
-        }
+// fonction qui permet de verifier si le nombre est premier
+function estPremier(n){
+ if(n<=1) return false
+
+ for(let i=2;i<Math.sqrt(n);i++){
+    if(n%i==0){
+        return false
     }
+ }
+
+ return true
 }
 
-nombrePremier([7, 3, 5, 2, 4, 2, 20, 50])
+function nombrePremier(tab){
+    const premier=[]
+    const nonPremiers=[]
+
+    for(let i=0;i<tab.length;i++){
+        if(estPremier(tab[i])){
+            premier.push(tab[i])
+        }else{
+            nonPremiers.push(tab[i])
+        }
+    }
+
+    console.log("Nombre premier",premier);
+    console.log("Nombre non premier",nonPremiers);
+    
+    
+}
+nombrePremier([7, 3, 5, 2, 4,  20, 50,9,100,121,122,31,71,81])
